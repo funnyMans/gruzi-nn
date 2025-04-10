ecbb7858dee4e8a20df24c28744735a03c1164bc
Vladimir <vl.petrosyan94@gmail.com>
2025-04-10 21:19:18 +0400

    fix/ Privacy_page_temp_fix

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,27 +1,26 @@
-import {
-  GetPrivacyPolicyQuery,
-  GetPrivacyPolicyQueryVariables,
-} from '@/shared/gql/client/__generated__/graphql';
-import { GET_PRIVACY_POLICY } from '@/shared/gql/client/privacy/privacyQueries';
+// import {
+//   GetPrivacyPolicyQuery,
+//   GetPrivacyPolicyQueryVariables,
+// } from '@/shared/gql/client/__generated__/graphql';
+// import { GET_PRIVACY_POLICY } from '@/shared/gql/client/privacy/privacyQueries';
 
-import { query } from '@/shared/providers/apollo-client/apollo-client-SSR';
-import React from 'react';
-import config from '@/shared/lib/config';
+// import { query } from '@/shared/providers/apollo-client/apollo-client-SSR';
+// import config from '@/shared/lib/config';
 
 const PrivacyPolicyPage = async () => {
-  const res = await query<
-    GetPrivacyPolicyQuery,
-    GetPrivacyPolicyQueryVariables
-  >({
-    query: GET_PRIVACY_POLICY,
-    variables: {
-      id: config.privacy.latestPrivacyPolicy,
-    },
-  });
+  // const res = await query<
+  //   GetPrivacyPolicyQuery,
+  //   GetPrivacyPolicyQueryVariables
+  // >({
+  //   query: GET_PRIVACY_POLICY,
+  //   variables: {
+  //     id: config.privacy.latestPrivacyPolicy,
+  //   },
+  // });
 
-  const privacy = res.data.privacyPolicy!;
+  // const privacy = res.data.privacyPolicy!;
 
-  return <div>PrivacyPolicyPage {privacy.id}</div>;
+  return <div>PrivacyPolicyPage </div>;
 };
 
 export default PrivacyPolicyPage;
