ec56cb7b06336259fe50281de8d43ed5783bf4a7
Vladimir <vl.petrosyan94@gmail.com>
2025-04-10 21:06:59 +0400

    fix/GNN_terms_of_use

--- src/app/terms/page.tsx
+++ src/app/terms/page.tsx
@@ -15,25 +15,16 @@ const TermsOfUsePage = async () => {
     );
   }
 
-  try {
-    const res = await query<GetTermsOfUseQuery, GetTermsOfUseQueryVariables>({
-      query: GET_TERMS_OF_USE,
-      variables: {
-        id: config.terms.latestTermsOfUse,
-      },
-    });
+  const res = await query<GetTermsOfUseQuery, GetTermsOfUseQueryVariables>({
+    query: GET_TERMS_OF_USE,
+    variables: {
+      id: config.terms.latestTermsOfUse,
+    },
+  });
 
-    const terms = res.data.termsOfUse;
+  const terms = res.data.termsOfUse!;
 
-    if (!terms) {
-      return <div>Error: Terms of Use not found.</div>;
-    }
-
-    return <div>TermsOfUsePage {terms.id}</div>;
-  } catch (error) {
-    console.error('Failed to fetch Terms of Use:', error);
-    return <div>Error: Failed to fetch Terms of Use.</div>;
-  }
+  return <div>TermsOfUsePage {terms.id}</div>;
 };
 
 export default TermsOfUsePage;
